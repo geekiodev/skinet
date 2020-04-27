dd58f5e50e59c9053f7184633ce4873d94185c7a
geekio <geekio.dev@gmail.com>
2020-04-27 01:45:11 -0400

    End of section 3

---
+++ Infrastructure/Data/Config/ProductConfiguration.cs
@@ -0,0 +1,21 @@
+using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Data.Config
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.Property(p => p.Id).IsRequired();
+            builder.Property(p => p.Name).IsRequired().HasMaxLength(100);
+            builder.Property(p => p.Description).IsRequired();
+            builder.Property(p => p.Price).HasColumnType("decimal(18,2)");
+            builder.Property(p => p.PictureUrl).IsRequired();
+            builder.HasOne(b => b.ProductBrand).WithMany().HasForeignKey(p => p.ProductBrandId); // Each product has one brand; Each brand can be affiliated to many products
+            builder.HasOne(b => b.ProductType).WithMany().HasForeignKey(p => p.ProductTypeId); // Each product has one product type; Each product type can be affiliated to many products
+            
+        }
+    }
+}
