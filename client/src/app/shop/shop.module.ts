717c4f944c7772878e839ce310344ebf9864c715
geekio <geekio.dev@gmail.com>
2020-05-22 18:39:06 -0400

    End of section 10

--- client/src/app/shop/shop.module.ts
+++ client/src/app/shop/shop.module.ts
@@ -4,16 +4,20 @@ import { ShopComponent } from './shop.component';
 import { ProductItemComponent } from './product-item/product-item.component';
 import { FormsModule } from '@angular/forms';
 import { SharedModule } from '../shared/shared.module';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { ShopRoutingModule } from './shop-routing.module';
 
 
 
 @NgModule({
-  declarations: [ShopComponent, ProductItemComponent],
+  declarations: [ShopComponent, ProductItemComponent, ProductDetailsComponent],
   imports: [
     CommonModule,
     FormsModule,
-    SharedModule
+    SharedModule,
+    ShopRoutingModule
   ],
-  exports: [ShopComponent]
+  exports: [
+  ]
 })
 export class ShopModule { }
